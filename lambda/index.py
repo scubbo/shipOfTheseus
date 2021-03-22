81d6c74833c21412d27baf627a050c78c5dccee1
Jack Jackson <scubbojj@gmail.com>
2021-03-21 20:12:35 -0700

    Improve logging from CustomResource

--- lambda/index.py
+++ lambda/index.py
@@ -3,32 +3,41 @@ import os
 import requests
 
 import logging
+# https://stackoverflow.com/a/45624044/1040915
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
 def handler(event, context):
     try:
-        log.info(f'DEBUG - called!')
-        log.info(f'{event}')
+        log.debug(f'DEBUG - called!')
+        log.info(f'Event: {event}')
         if event['RequestType'] in ['Create', 'Update']:
-            log.info(os.environ)
-            log.info(os.environ['githubCommitsUrl'])
-            log.info(requests.get(os.environ['githubCommitsUrl']).content)
+            log.debug(os.environ)
+            log.debug(os.environ['githubCommitsUrl'])
+            log.debug(requests.get(os.environ['githubCommitsUrl']).content)
             # TODO - pass in the commit that the pipeline's working on, and restrict this
             # to only "commits prior to that", just in case there are two commits in quick succession
             # TODO - write the data to `commits.json` in the S3 bucket.
         else:
             log.info('Non-create/update RequestType')
-        requests.put(event['ResponseURL'], data=dict(
+        responseData = dict(
             {'Status': 'SUCCESS'},
             **{key: event.get(key, '') for key in
                ['PhysicalResourceId', 'StackId', 'RequestId', 'LogicalResourceId']}
-        ))
+        )
+        log.debug(f'Response data: {responseData}')
+        requests.put(event['ResponseURL'], data=responseData)
     except Exception as e:
         log.error(f'Lambda failed! {e}')
-        requests.put(event['ResponseURL'], data=dict(
+        responseData = dict(
             {'Status': 'FAILED'},
             **{key: event.get(key, '') for key in
                ['PhysicalResourceId', 'StackId', 'RequestId', 'LogicalResourceId']}
-        ))
+        )
+        log.debug(f'Response data: {responseData}')
+        requests.put(event['ResponseURL'], data=responseData)
