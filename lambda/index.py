c440b7a6c236f21e0e4ac8e64bfd6518600891f3
Jack Jackson <scubbojj@gmail.com>
2021-03-22 19:52:43 -0700

    Implement commit-fetching logic

--- lambda/index.py
+++ lambda/index.py
@@ -10,15 +10,18 @@ log.setLevel(logging.DEBUG)
 
 def handler(event, context):
     try:
-        log.debug(f'DEBUG - called!')
         log.info(f'Event: {event}')
+        log.info(f'Logical Resource: {context.log_stream_name}')
         if event['RequestType'] in ['Create', 'Update']:
-            log.debug(os.environ)
-            log.debug(os.environ['githubCommitsUrl'])
-            log.debug(requests.get(os.environ['githubCommitsUrl']).content)
+            commit_data = _fetch_commit_history(os.environ['githubCommitsUrl'])
+            s3 = boto3.resource('s3')
+            bucket_name = os.environ['bucketArn'].split(':')[-1]
+            object = s3.Object(bucket_name, 'commits.json')
+            # TODO - do we need public ACL, or expiry?
+            object.put(Body=json.dumps(commit_data).encode('utf-8'))
+            log.info('Successfully uploaded commit data to `commits.json`')
             # TODO - pass in the commit that the pipeline's working on, and restrict this
             # to only "commits prior to that", just in case there are two commits in quick succession
-            # TODO - write the data to `commits.json` in the S3 bucket.
         else:
             log.info('Non-create/update RequestType')
 
@@ -51,3 +54,24 @@ def handler(event, context):
         )
         log.debug(f'Response data: {responseData}')
         requests.put(event['ResponseURL'], data=responseData, headers={'Content-Type': ''})
+
+
+def _fetch_commit_history(ghUrl):
+    commit = _get_commit_info(ghUrl)
+    commits = [commit]
+    while commit['parents']:
+        commit = _get_commit_info(commit['parent_url'])
+        commits.append(commit)
+    return commits
+
+def _get_commit_info(commit_url):
+    """
+    This assumes a strictly linear Git history (i.e. each commit except the first one has exactly
+    one parent), because Real Programmers use ~~butterflies~~ rebases. Fite me IRL, Git-nerds.
+    """
+    commit = requests.get(commit_url).json()
+    return {
+        'sha': commit['sha'],
+        'message': commit['commit']['message'],
+        'parent_url': commit['parents'][0]['url']
+    }
