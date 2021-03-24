da6036a3cd80dffcb6543fc48edfa44a50e260c3
Jack Jackson <scubbojj@gmail.com>
2021-03-24 13:53:17 -0700

    Parse the JSON, JSON-parser

--- static-site/js/main.js
+++ static-site/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.get('/commits.json', function(data) {
         $('#commits').append('<ul id="commits-list">')
-        $.each(data, function(idx, commit) {
+        $.each(JSON.parse(data), function(idx, commit) {
             $('#commits-list').append('<li>' + commit['sha'].slice(0, 6) + '\t' + commit['message'].slice(0, 50) + '</li>')
         });
         $('#commits').append('</ul>');
